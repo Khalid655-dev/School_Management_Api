7edba61fe19cf5941c54a368863bca0aea4184e7
khalid655_dev <khalidazizdirlower@gmail.com>
2022-06-17 12:22:34 +0500

    Added Reset Password API for Teacher

--- routers/auth.py
+++ routers/auth.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
+import utils
 from database import get_db
 from utils import verify
 import oauth2
 from auth_token.schemas import Token
 from models import Admin, Teacher
+from teacher.schemas import TeacherResetPassword
 
 
 auth_router = APIRouter(tags=["Authentication"])
@@ -52,6 +54,19 @@ def login_teacher(teacher_credentials: OAuth2PasswordRequestForm = Depends(), db
     access_token = oauth2.create_access_token(data={"teacher_id": teacher.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@auth_router.patch("/resetpasswords/{id_}", status_code=status.HTTP_201_CREATED)
+def reset_teacher_password(passwordreset: TeacherResetPassword, id_: int, db: Session = Depends(get_db)):
+    teacher = db.query(Teacher).filter(Teacher.id == id_).first()
+
+    hashed_password = utils.hash_(passwordreset.password)
+    passwordreset.password = hashed_password
+
+    teacher.password = hashed_password
+    db.commit()
+    db.refresh(teacher)
+    return {"message": "Your password has been Changed Successfully!!"}
 
 
  
